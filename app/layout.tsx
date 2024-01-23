0008b245b75f1cb81f24d217c764dfab76f279c5
Ethan Flores <ethanflores@Ethans-MacBook-Pro.local>
2024-01-23 14:00:45 -0800

    vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import Header from "@/components/header";
 import ActiveSectionContextProvider from "@/context/active-section-context";
 import GoogleTagManager from "./google-tag-manager";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -36,6 +37,7 @@ export default function RootLayout({
         <ActiveSectionContextProvider>
           <Header />
           {children}
+          <Analytics />
         </ActiveSectionContextProvider>
       </body>
     </html>
